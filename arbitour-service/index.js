e8638352d8413a9dbfc8bc412afc00f2de52ab19
Himesh Ramjee <himeshramjee@users.noreply.github.com>
2020-09-13 21:40:23 +0200

    Basic dockerization so I can deploy to NoddyX.

--- arbitour-service/index.js
+++ arbitour-service/index.js
@@ -36,31 +36,31 @@ app.get("/:currencyPair/orderbook/:entryCount", async (req, res) => {
   res.status(200).send(orderBookData);
 });
 
-// app.get("/orders/open", async (req, res) => {
-//   const openOrders = [{ 
-//     orderId: "",
-//     customerOrderId: "",
-//     createdAt: "",
-//     side: "",
-//     currencyPair: "",
-//     price: ""
-//   }];
+app.get("/orders/open", async (req, res) => {
+  const openOrders = [{ 
+    orderId: "",
+    customerOrderId: "",
+    createdAt: "",
+    side: "",
+    currencyPair: "",
+    price: ""
+  }];
 
-//   await axios.get(`https://api.valr.com/v1/orders/open`, { timeout: 500 })
-//     .then(response => {
-//       const ordersData = response.data;
+  await axios.get(`https://api.valr.com/v1/orders/open`, { timeout: 500 })
+    .then(response => {
+      const ordersData = response.data;
 
-//       ordersData.map(order => {
-//         openOrders.push(order);
-//       });
-//     })
-//     .catch(error => {
-//       console.log(`Failed to retrieve open orders data. Error: ` + error);
-//       // debuglog(error);
-//     });
+      ordersData.map(order => {
+        openOrders.push(order);
+      });
+    })
+    .catch(error => {
+      console.log(`Failed to retrieve open orders data. Error: ` + error);
+      // debuglog(error);
+    });
 
-//   res.status(200).send(openOrders);
-// });
+  res.status(200).send(openOrders);
+});
 
 app.listen("9000", () => {
   console.log("Arbitour v0.0.1: Listening on port 9000");
